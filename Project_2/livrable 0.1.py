bd1f8ee70f133c15f8dcfe77f4b8f60809b643c9
joluhan <135083872+joluhan@users.noreply.github.com>
2023-05-31 14:09:54 +0700

    added input file location + name file

--- Project_2/livrable 0.1.py
+++ Project_2/livrable 0.1.py
@@ -1,4 +1,4 @@
-# écrire un script Python qui visite page et extrait les informations détaillées dans le document des exigences.
+# ============================== Étape 2 : Extraire les données d’un seul produit ==============================
 
 import requests  # librairy to make HTTP requests and interact with web ressources
 from bs4 import (
@@ -7,7 +7,7 @@ from bs4 import (
 import csv  # Import the csv module for reading and writing CSV files
 import os  # Import the os module for operating system-related functionalities
 
-url = "http://books.toscrape.com/"  # variable creation of website I want to scrape
+url = "http://books.toscrape.com/catalogue/its-only-the-himalayas_981/index.html"  # variable creation of page to be scraped
 
 response = requests.get(url)  # variable requesting opening the website
 
@@ -23,26 +23,25 @@ response_content = BeautifulSoup(
     response.content, "html.parser"
 )  # create a BeautifulSoup object by passing in the response content while specifying the parser to use
 
+book_container = response_content.find(
+    "article", class_="product_page"
+)  # Find the book container for the specific book
 
-book_containers = response_content.find_all(
-    "article", class_="product_pod"
-)  # Find all the book containers
-
-book_data = []  # Initialize a list to store the extracted data
-
-for container in book_containers:  # Iterate over each book container
-    # Extract the title and price for each book
-    title = container.h3.a.text
-    price = container.find("p", class_="price_color").text
-
-    book_data.append({"Title": title, "Price": price})
+# Extract the title and price for the book
+title_element = book_container.find("h1")
+title = title_element.text.strip() if title_element else ""
+price_element = book_container.find("p", class_="price_color")
+price = price_element.text.strip() if price_element else ""
+book_data = [{"Title": title, "Price": price}]
 
 folder_path = input(
     "Enter the desired folder path: "
 )  # Prompt the user to enter the desired folder path
 
+name_file_result = input("Give a name to the file: ")
+
 filename = os.path.join(
-    folder_path, "book_data.csv"
+    folder_path, name_file_result + ".csv"
 )  # Define filename for the CSV file
 
 # Write the data to the CSV file
@@ -53,5 +52,4 @@ with open(filename, "w", newline="") as csvfile:
     writer.writerows(book_data)
 
 print(f"Data extraction successful. Saved as {filename}")  # Print success message
-
 print(f"Location: {os.path.abspath(filename)}")  # Print location of data file
