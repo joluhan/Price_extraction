2ef9a5b1365ef3923c200637718aaa29d845a00f
joluhan <135083872+joluhan@users.noreply.github.com>
2023-06-15 14:51:53 +0700

    Debugged script - working

--- Project_2/livrable 0.2.py
+++ Project_2/livrable 0.2.py
@@ -1,23 +1,18 @@
 # ============================== Étape 2 : Extraire les données d’une catégorie ==============================
 
-# process:
-# https://www.crummy.com/software/BeautifulSoup/bs4/doc/#find
-# https://www.geeksforgeeks.org/how-to-get-the-next-page-on-beautifulsoup/
-
-
 import requests
 from bs4 import BeautifulSoup
 import csv
 import os
 
-# url joining librairies
+# url joining libraries
 from urllib.parse import urljoin
 
 # Target URL and base URL
 catalogue_url = (
     "https://books.toscrape.com/catalogue/category/books/travel_2/index.html"
 )
-base_url = "https://books.toscrape.com/catalogue/"
+base_url = "https://books.toscrape.com/"
 
 # Send a GET request to the URL
 response = requests.get(catalogue_url)
@@ -30,153 +25,120 @@ else:
 
 # Parse the response content with BeautifulSoup
 soup = BeautifulSoup(response.content, "html.parser")
-
-# >>>>============= test =============
-
-# collect url of each book
-
-# Initialize an empty list to store the book URLs
-book_data = []
+# print(soup.prettify())
 
 
-# Find all book articles on the webpage
-def find_all_book_articles(soup):
-    book_articles = soup.findAll("li", class_="col-xs-6 col-sm-4 col-md-3 col-lg-3")
-    for article in book_articles:
-        book_url = article.find("a", href=True)["href"]
-        # join url with the base URL of the website
-        book_url = urljoin(base_url, book_url)
-        book_data.append(book_url)
+# >>>>==========================WORKING============================
+# Function ==========> urls extraction
+def extract_urls(catalogue_url):
+    # Fetch the page content
+    response = requests.get(catalogue_url)
+    html_content = response.content
 
-        print(book_data)
+    # Parse the HTML
+    soup = BeautifulSoup(html_content, "html.parser")
 
+    # Find and extract every URL
+    base_url = response.url  # Get the base URL of the page
+    urls = []
+    articles = soup.find_all("article", class_="product_pod")
+    for article in articles:
+        href = article.find("h3").find("a")["href"]
+        absolute_url = urljoin(base_url, href)  # Convert relative URL to absolute URL
+        urls.append(absolute_url)
 
-find_all_book_articles(soup, book_data)
-# # Iterate over each book article and collect the URLs
-# for book_article in book_data:
-#     # Find the anchor tag within the book article
-#     if anchor_tag:
-#         # Extract the relative URL
-#         relative_url = anchor_tag
-#         # Construct the absolute URL using the base URL of the website
-#         absolute_url = urljoin(base_url, relative_url)
-#         # Append the absolute URL to the book_data list
-#         book_data.append(absolute_url)
-#         # Print the absolute URL (optional)
-#         print(absolute_url)
-#     else:
-#         # Handle cases where the anchor tag is not found
-#         print("Anchor tag not found for a book article")
-
-# extaire données de chaque url
+    return urls
 
 
-# sauvegarder les données
+result = extract_urls(catalogue_url)
+# print(result)
 
 
-# ============= test =============<<<<
+# Function ==========> data extraction
+def extract_data(result):
+    book_data = []
 
-# # Category URL
-# category_url = "catalogue/category/books/travel_2/index.html"
-# # Book URL
-# book_url = "catalogue/a-light-in-the-attic_1000/index.html"
-# # Join the URLs
+    for url in result:
+        response = requests.get(url)
+        html_content = response.content
+        soup = BeautifulSoup(html_content, "html.parser")
 
+        product_page_url = url
+        universal_product_code = (
+            soup.find("table", class_="table-striped").find("td").text
+        )
+        title = soup.find("div", class_="product_main").find("h1").text
+        price_including_tax = soup.find("p", class_="price_color").text.strip("£")
+        price_excluding_tax = soup.find_all("p", class_="price_color")[1].text.strip(
+            "£"
+        )
+        number_available = soup.find("p", class_="instock availability").text.strip()
+        product_description = (
+            soup.find("article", class_="product_page")
+            .find("p", recursive=False)
+            .text.strip()
+        )
+        category = soup.find("ul", class_="breadcrumb").find_all("a")[2].text
+        review_rating = soup.find("p", class_="star-rating")["class"][1]
+        image_url = urljoin(
+            url, soup.find("div", class_="item active").find("img")["src"]
+        )
 
-# # Loop to scrape data from multiple pages
-# while True:
-#     # Request the URL of each page
-#     response = requests.get(absolute_url)
+        book_data.append(
+            {
+                "Pdt url": product_page_url,
+                "UPC": universal_product_code,
+                "Title": title,
+                "Price incl VAT": price_including_tax,
+                "Price excl VAT": price_excluding_tax,
+                "Nb available": number_available,
+                "Pdt description": product_description,
+                "Category": category,
+                "Review Rating": review_rating,
+                "Img url": image_url,
+            }
+        )
 
-#     soup_url = BeautifulSoup(response, "html.parser")
-#     # Check if the request was successful
-#     if response.status_code == 200:
-#         print(f"Access to {absolute_url} successful")
-#         continue
-
-#     # Find all book containers
-#     container = soup_url.find_all("article", class_="product_pod")
-
-#     # ===============use tr/th to find the data =================================
-#     # Iterate through each book container to extract data
-#     for container in response:
-#         # Extract the desired information
-
-#         product_page_url = absolute_url
-#         universal_product_code = (
-#             soup.find("th", string="UPC").find_next_sibling("td").string
-#         )
-#         title = soup.find("h1").string
-#         price_including_tax = (
-#             soup.find("th", string="Price (incl. tax)").find_next_sibling("td").string
-#         )
-#         price_excluding_tax = (
-#             soup.find("th", string="Price (excl. tax)").find_next_sibling("td").string
-#         )
-#         number_available = (
-#             soup.find("th", string="Availability").find_next_sibling("td").string
-#         )
-#         product_description = (
-#             soup.find("div", {"id": "product_description"})
-#             .find_next("p")
-#             .string.strip()
-#         )
-#         category = soup.find("a", href="../category/books/poetry_23/index.html").string
-
-#         review_rating = soup.find("p", class_="star-rating")["class"][1]
-#         image_url = urljoin(base_url, soup.img["src"])
-#         # ===============use tr/th to find the data =================================
-
-#         # Append the extracted data as a dictionary to the book_data list
-#         book_data.append(
-#             {
-#                 "Pdt url": product_page_url,
-#                 "UPC": universal_product_code,
-#                 "Title": title,
-#                 "Price incl VAT": price_including_tax,
-#                 "Price excl VAT": price_excluding_tax,
-#                 "Nb available": number_available,
-#                 "Pdt description": product_description,
-#                 "Category": category,
-#                 "Review Rating": review_rating,
-#                 "Img url": image_url,
-#             }
-#         )
-
-#     # Find the next page button
-#     next_button = soup.find("li", class_="next")
-#     if next_button is None:
-#         break
-
-#     # Get the URL for the next page
-#     next_page_url = urljoin(base_url, next_button.a["href"])
-#     next_page_response = requests.get(next_page_url)
-#     soup = BeautifulSoup(next_page_response.content, "html.parser")
+    return book_data
 
 
-# # Define fieldnames for CSV header
-# fieldnames = [
-#     "Pdt url",
-#     "UPC",
-#     "Title",
-#     "Price incl VAT",
-#     "Price excl VAT",
-#     "Nb available",
-#     "Pdt description",
-#     "Category",
-#     "Review Rating",
-#     "Img url",
-# ]
+# Pass the result of extract_urls() to extract_data()
+book_data = extract_data(result)
 
-# # Specify the directory, filename, and file path for the CSV
-# directory = r"C:\Users\johan\Desktop"
-# filename = "book_data.csv"
-# filepath = os.path.join(directory, filename)
 
-# # Write data to the CSV file
-# with open(filepath, "w", newline="", encoding="utf-8") as csvfile:
-#     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-#     writer.writeheader()
-#     writer.writerows(book_data)
+# Function ==========> save data to csv file
+def save_data_to_csv(book_data, directory, filename):
+    fieldnames = [
+        "Pdt url",
+        "UPC",
+        "Title",
+        "Price incl VAT",
+        "Price excl VAT",
+        "Nb available",
+        "Pdt description",
+        "Category",
+        "Review Rating",
+        "Img url",
+    ]
+    filepath = os.path.join(directory, filename)
 
-# print(f"Data has been successfully saved to {filepath}")
+    with open(filepath, "w", newline="", encoding="utf-8") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(book_data)
+
+    print(f"Data has been successfully saved to {filepath}")
+
+
+# >>>>==========================WORKING============================
+
+
+# >>>>==========================TEST============================
+
+# ==========================TEST============================<<<<<
+
+
+# Example usage
+directory = r"C:\Users\johan\Desktop"
+filename = "book_data.csv"
+save_data_to_csv(book_data, directory, filename)
