77f0ae5c6e58f7d8b50b333dec0ef9c72ad00ac4
joluhan <135083872+joluhan@users.noreply.github.com>
2023-06-20 16:41:06 +0700

    Update livrable 0.31.py

--- Project_2/livrable 0.31.py
+++ Project_2/livrable 0.31.py
@@ -18,25 +18,32 @@ base_url = "https://books.toscrape.com/catalogue/page-1.html"
 
 
 def category_urls_list(base_url):
-    category_urls_list = []
+    category_urls_list = []  # Create an empty list to store category URLs
 
-    response = requests.get(base_url)
-    html_content = response.content
-    soup = BeautifulSoup(html_content, "html.parser")
+    response = requests.get(base_url)  # Send a GET request to the specified URL
+    html_content = response.content  # Get the content of the response
+    soup = BeautifulSoup(html_content, "html.parser")  # Parse the HTML content
 
-    category_list = soup.find("ul", class_="nav-list")
-    category_items = category_list.find_all("li")
+    category_list = soup.find("ul", class_="nav-list")  # Find the category list element
+    category_items = category_list.find_all(
+        "li"
+    )  # Find all list items within the category list
 
     for index, category_item in enumerate(category_items):
         if index > 0:  # Start appending from the second element
-            category_url = urljoin(base_url, category_item.find("a")["href"])
-            category_urls_list.append(category_url)
+            category_url = urljoin(
+                base_url, category_item.find("a")["href"]
+            )  # Get the URL of the category item
+            category_urls_list.append(
+                category_url
+            )  # Append the category URL to the list
 
-    return category_urls_list
+    return category_urls_list  # Return the list of category URLs
 
 
+# Call the function to get the list of category URLs
 list_urls = category_urls_list(base_url)
-# print(list_urls)
+# /print(list_urls)  # Print the list of category URLs
 # # ==========================TEST============================<<<<<<<
 
 
