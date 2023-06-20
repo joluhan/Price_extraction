0f4708844b502e72cfe81fba1b49faa53552cf7c
joluhan <135083872+joluhan@users.noreply.github.com>
2023-06-20 16:14:05 +0700

    Update livrable 0.31.py

--- Project_2/livrable 0.31.py
+++ Project_2/livrable 0.31.py
@@ -13,6 +13,23 @@ from urllib.parse import urljoin
 # A= Create a loop that collect all url of each category
 # # >>>>>>==========================TEST============================
 # extract all urls of each category
+base_url = "https://books.toscrape.com/catalogue/page-1.html"
+
+
+def category_urls_list(base_url):
+    category_urls_list = []
+
+    # Send a GET request to the specified URL
+    response = requests.get(base_url)
+    # Parse the HTML
+    soup = BeautifulSoup(response, "html.parser")
+
+    url_category_extraction = (
+        soup.find("ul", {"class": "nav nav-list"}).find("li").find("a").find_all("a")
+    )
+    for url_category in url_category_extraction:
+        category_urls_list.append(url_category.find("a")["href"])
+
 
 # # ==========================TEST============================<<<<<<<
 
@@ -20,47 +37,6 @@ from urllib.parse import urljoin
 # # >>>>>>==========================TODO============================
 # B= create another loop that get the book url in each category
 # # >>>>>>==========================TEST============================
-# Function ==========> urls extraction
-def extract_urls():
-    urls = []
-    while True:
-        # Send a GET request to the specified URL
-        response = requests.get(url_home)
-
-        # Retrieve the content of the response
-        html_content = response.content
-        # Check if the request was successful
-        if response.status_code == 200:
-            print(f"Access to {url_home} successful")
-        else:
-            print(f"Access to {url_home} unsuccessful")
-
-        # Parse the HTML
-        soup = BeautifulSoup(html_content, "html.parser")
-
-        # Find and extract every URL
-        url_home = response.url  # Get the base URL of the page
-
-        # Find all article elements with the specified class
-        articles = soup.find_all("article", class_="product_pod")
-        for article in articles:
-            # Find the href attribute of the nested <a> element
-            href = article.find("h3").find("a")["href"]
-            absolute_url = urljoin(
-                url_home, href
-            )  # Convert relative URL to absolute URL
-            urls.append(absolute_url)  # Add the absolute URL to the list of URLs
-
-        # Find the next page link
-        next_link = soup.find("li", class_="next")
-        if next_link is None:
-            break  # Exit the loop if there is no next page
-
-        # Get the URL for the next page
-        next_page_url = urljoin(url_home, next_link.find("a")["href"])
-        url_home = next_page_url
-
-    return urls
 
 
 # # ==========================TEST============================<<<<<<<
@@ -68,9 +44,7 @@ def extract_urls():
 
 # # >>>>>>==========================TODO============================
 # and put them in a list and then use a for loop to extract the data and save them is a csv file
-# # ==========================TODO============================<<<<<<<
-
-
 # # >>>>>>==========================TEST============================
+
 
 # # ==========================TEST============================<<<<<<<
