52e5638102381ab59ee07feda82f909b6918ee31
joluhan <135083872+joluhan@users.noreply.github.com>
2023-06-20 17:01:16 +0700

    Update livrable 0.31.py

--- Project_2/livrable 0.31.py
+++ Project_2/livrable 0.31.py
@@ -1,22 +1,12 @@
-# ============================== Étape 3 : Extraire les données du site ==============================
-
 import requests
 from bs4 import BeautifulSoup
-
 import csv
 import os
 import re
 from unicodedata import normalize
 from urllib.parse import urljoin
 
-
-# # >>>>>>==========================TODO============================
-# A= Create a loop that collect all url of each category
-# # >>>>>>==========================TEST============================
-# extract all urls of each category
-base_url = "https://books.toscrape.com/catalogue/page-1.html"
-
-
+# Function to extract category URLs
 def category_urls_list(base_url):
     category_urls_list = []  # Create an empty list to store category URLs
 
@@ -42,53 +32,40 @@ def category_urls_list(base_url):
 
 
 # Call the function to get the list of category URLs
+base_url = "https://books.toscrape.com/catalogue/page-1.html"
 list_urls = category_urls_list(base_url)
-# /print(list_urls)  # Print the list of category URLs
-# # ==========================TEST============================<<<<<<<
 
-
-# # >>>>>>==========================TODO============================
-# B= create another loop that get the book url in each category
-# # >>>>>>==========================TEST============================
-# Function ==========> urls extraction
+# Function to extract book URLs from each category
 def extract_urls(list_urls):
     urls = []
 
     for url in list_urls:
-        # Send a GET request to the specified URL
-        response = requests.get(url)
+        response = requests.get(url)  # Send a GET request to the specified URL
+        html_content = response.content  # Retrieve the content of the response
 
-        # Retrieve the content of the response
-        html_content = response.content
-        # Check if the request was successful
-        if response.status_code == 200:
+        if response.status_code == 200:  # Check if the request was successful
             print(f"Access to {url} successful")
         else:
             print(f"Access to {url} unsuccessful")
 
-        # Parse the HTML
-        soup = BeautifulSoup(html_content, "html.parser")
+        soup = BeautifulSoup(html_content, "html.parser")  # Parse the HTML content
 
-        # Find and extract every URL
         url_home = response.url  # Get the base URL of the page
 
         # Find all article elements with the specified class
         articles = soup.find_all("article", class_="product_pod")
         for article in articles:
-            # Find the href attribute of the <a> element
-            href = article.find("h3").find("a")["href"]
+            href = article.find("h3").find("a")["href"]  # Find the href attribute of the <a> element
             absolute_url = urljoin(
                 url_home, href
             )  # Convert relative URL to absolute URL
             urls.append(absolute_url)  # Add the absolute URL to the list of URLs
 
-        # Find the next page link
-        next_link = soup.find("li", class_="next")
+        next_link = soup.find("li", class_="next")  # Find the next page link
         if next_link is None:
             break  # Exit the loop if there is no next page
 
-        # Get the URL for the next page
-        next_page_url = urljoin(url_home, next_link.find("a")["href"])
+        next_page_url = urljoin(url_home, next_link.find("a")["href"])  # Get the URL for the next page
         list_urls.append(next_page_url)  # Append the next page URL to the list of URLs
 
     return urls
@@ -96,58 +73,27 @@ def extract_urls(list_urls):
 
 book_urls = extract_urls(list_urls)
 
-# /print(extract_urls(list_urls))
-
-# # ==========================TEST============================<<<<<<<
-
-
-# # >>>>>>==========================TODO============================
-# and put them in a list and then use a for loop to extract the data and save them is a csv file
-# # >>>>>>==========================TEST============================
-# Function ==========> extract data from a list of URLs
+# Function to extract data from a list of URLs
 def extract_data(book_urls):
     book_data = []  # Initialize an empty list to store book data
 
-    for url in book_urls:  # Iterate over each URL in the input list
+    for url in book_urls:
         response = requests.get(url)  # Send a GET request to the URL
-
         html_content = response.content  # Get the content of the response
-
-        # Create a BeautifulSoup object for parsing HTML
-        soup = BeautifulSoup(html_content, "html.parser")
+        soup = BeautifulSoup(html_content, "html.parser")  # Create a BeautifulSoup object for parsing HTML
 
         # Extract various data from the HTML using BeautifulSoup .find
         product_page_url = url
-        universal_product_code = (
-            soup.find("table", class_="table-striped").find("td").text
-        )
+        universal_product_code = soup.find("table", class_="table-striped").find("td").text
         title = soup.find("div", class_="product_main").find("h1").text
+        price_including_tax = soup.find("th", string="Price (incl. tax)").find_next_sibling("td").string.strip("£")
+        price_excluding_tax = soup.find("th", string="Price (excl. tax)").find_next_sibling("td").string.strip("£")
 
-        price_including_tax = (
-            soup.find("th", string="Price (incl. tax)").find_next_sibling("td").string
-        ).text.strip("£")
-        price_excluding_tax = (
-            soup.find("th", string="Price (excl. tax)").find_next_sibling("td").string
-        ).text.strip("£")
-
-        # Extract availability text
         availability_text = soup.find("p", class_="instock availability").text.strip()
-        # Extract the number from the availability text using regular expressions and string manipulation
-        match = re.search(
-            r"\d+", availability_text
-        )  # Search for any sequence of digits
-        if match:
-            number_available = match.group()  # Extract the matched digits
-        else:
-            number_available = None  # Set to None if no match is found
+        match = re.search(r"\d+", availability_text)  # Search for any sequence of digits
+        number_available = match.group() if match else None
 
-        # extract product description
-        extracted_text = (
-            soup.find("div", {"id": "product_description"})
-            .find_next("p")
-            .string.strip()
-        )
-        # normalize product description by encoding/decoding the extracted data
+        extracted_text = soup.find("div", {"id": "product_description"}).find_next("p").string.strip()
         product_description = (
             normalize("NFKD", extracted_text)
             .encode("ascii", "ignore")
@@ -157,9 +103,7 @@ def extract_data(book_urls):
 
         category = soup.find("ul", class_="breadcrumb").find_all("a")[2].text
         review_rating = soup.find("p", class_="star-rating")["class"][1]
-        image_url = urljoin(
-            url, soup.find("div", class_="item active").find("img")["src"]
-        )
+        image_url = urljoin(url, soup.find("div", class_="item active").find("img")["src"])
 
         # Create a dictionary with the extracted data and append it to the book_data list
         book_data.append(
@@ -180,14 +124,10 @@ def extract_data(book_urls):
     return book_data  # Return the list of extracted book data
 
 
-# Pass the result of extract_urls() to extract_data()
 book_data = extract_data(book_urls)
 
-
-# # ==========================TEST============================<<<<<<<
-# Function ==========> save data to a CSV file
+# Function to save data to a CSV file
 def save_data_to_csv(book_data, directory, filename):
-    # Define the field names for the CSV file
     fieldnames = [
         "Pdt url",
         "UPC",
@@ -200,27 +140,26 @@ def save_data_to_csv(book_data, directory, filename):
         "Review Rating",
         "Img url",
     ]
-    # Create the file path by joining the directory and filename
-    filepath = os.path.join(directory, filename)
+    filepath = os.path.join(directory, filename)  # Create the file path by joining the directory and filename
 
-    # Open the CSV file in write mode with UTF-8 encoding
     with open(filepath, "w", newline="", encoding="utf-8") as csvfile:
-        # Create a CSV writer object using the field names
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        # Write the header row with the field names
-        writer.writeheader()
-        # Write the data rows to the CSV file
-        writer.writerows(book_data)
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)  # Create a CSV writer object using the field names
+        writer.writeheader()  # Write the header row with the field names
+        writer.writerows(book_data)  # Write the data rows to the CSV file
 
-    # Print a success message with the file path
     print(f"Data has been successfully saved to {filepath}")
 
 
-# Define the directory where the file will be saved
-directory = r"C:\Users\johan\Desktop"
-
-# Specify the filename for the CSV file
-filename = "book_data02.csv"
-
-# Call the function to save the book_data to a CSV file in the specified directory
-save_data_to_csv(book_data, directory, filename)
+# Iterate over each category URL
+for category_url in list_urls:
+    category_name = category_url.split("/")[-2]  # Extract the category name from the URL
+    
+    category_directory = os.path.join(directory, category_name)  # Create a directory for the category if it doesn't exist
+    if not os.path.exists(category_directory):
+        os.makedirs(category_directory)
+    
+    category_book_urls = extract_urls([category_url])  # Extract book URLs for the current category
+    category_book_data = extract_data(category_book_urls)  # Extract book data for the current category
+    
+    category_filename = f"{category_name}_book_data.csv"  # Specify the filename for the category book data
+    save_data_to_csv(category_book_data, category_directory, category_filename)  # Save the category book data to a CSV file
